58df5558c5473c18478ac51d372fd7ca02034f7b
Sharol Chand <sharolchand@gmail.com>
2016-06-13 20:18:21 -0700

    disconnected users appear grey

--- server.js
+++ server.js
@@ -11,28 +11,18 @@ app.set('view engine', 'ejs');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(session({secret: 'codingdojorocks'}));
+// app.use(session({secret: 'codingdojorocks'}));
 
 
 /****** variables *****/
 var users = {};
+var user_count = 0;
 var messages = [];
 /****** end vars *****/
 
 /***** Routes ******/
 app.get('/', function(req, res){
-    // session
-    req.session.test = 'Group Chat';
-    var sid = req.sessionID;
-    var prompt = false;
-
-    // console.log(users);
-    // if(!users[sid]){
-    //     prompt = false;
-    // }else{
-    //     prompt = true;
-    // }
-    res.render('index', {sid : sid, prompt: prompt});
+    res.render('index');
 })
 
 // server stuff
@@ -43,36 +33,36 @@ var server = app.listen(8000, function(){
 // socket stuff
 var io = require('socket.io').listen(server);
 
-
-
 io.sockets.on('connection', function(socket){
-    // console.log(socket.id);
+    console.log(socket.id);
 
     // Listening:
     socket.on('got_new_user', function(data){
-        users[data.sid] = data.name;
-        io.emit('new_user', {name: data.name, uId: data.sid});
-        socket.emit('existing_users', {existing_users: users});
-        io.emit('new_message_added', {messages: messages});
+        user_count++;
+        users[socket.id] = {user_count: user_count, name: data.name};
 
+        // users[socket.id] = data.name;
+        // users[user_count] = {name: data.name, id: socket.id}
+
+        io.emit('new_user', {name: data.name, uId: socket.id, user_count: user_count});
+        socket.emit('existing_users', {existing_users: users, uID: socket.id});
+        io.emit('new_message_added', {messages: messages});
     });
 
     socket.on('new_message', function(data){
-
         messages.push(data);
-console.log(messages);
+
         socket.emit('message_added');
         io.emit('new_message_added', {messages: messages});
-        // socket.emit('existing_users', {existing_users: users});
     });
 
-    // socket.on("disconnect", function(data){
-    //     // req.session.test = 'Group Chat';
-    //     // var sid = req.sessionID;
-    //     console.log("user disconnected", users);
-    //     io.emit('disconnect_user', {name: data.name, uId: data.sid});
-    //
-    // })
+    socket.on("disconnect", function(data){
+        // console.log(socket.id);
+        var sid = socket.id;
+
+        io.emit('disconnect_user', {disconnect_id: socket.id});
+
+    })
     // app.io.route('got_new_user', function(req,res){
     //     app.io.broadcast('new_user', {new_user_name: req.data.name})
     //     req.io.emit('existing_user', users);
